916d4663d80d4e3b9541bba4488a47e203e31eba
Lawson Feltman <john.lawson.feltman@gmail.com>
2014-08-26 10:09:05 -0400

    first project commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+    	$('.ryu-still').hide();
+		$('.ryu-ready').show();
+  		})
+	.mouseleave(function() {
+    	$('.ryu-ready').hide();
+    	$('.ryu-still').show();
+  		})
+	.mousedown(function() {
+    	playHadouken();
+    	$('.ryu-ready').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').show();
+    	$('.hadouken').finish().show()
+  			.animate(
+    			{'left': '1200px'},
+    			500,
+    			function() {
+      				$(this).hide();
+      				$(this).css('left', '425px');
+    			}
+  			);
+  		})
+    .mouseup(function() {
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
+  		});
+    $('html').keydown(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-still').hide();
+            $('.ryu-cool').show();
+        }
+    })
+    .keyup(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-still').show();
+            $('.ryu-cool').hide();
+        };
+    })
+})
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
